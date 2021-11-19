0a5ca3fadd5f1bf1e012242edd74cb8c95a2896d
Yan <ssffggdd@mail.ru>
2021-11-19 21:06:09 +0300

    revert preetier

--- src/TodoItemForm.tsx
+++ src/TodoItemForm.tsx
@@ -1,84 +1,87 @@
-import { useTodoItems } from "./TodoItemsContext";
-import { useForm, Controller } from "react-hook-form";
-import FormControl from "@material-ui/core/FormControl";
-import InputLabel from "@material-ui/core/InputLabel";
-import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
-import { makeStyles } from "@material-ui/core/styles";
-import Select from "@material-ui/core/Select";
-import MenuItem from "@material-ui/core/MenuItem";
+import { useTodoItems } from './TodoItemsContext';
+import { useForm, Controller } from 'react-hook-form';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 
 const useInputStyles = makeStyles(() => ({
-  root: {
-    marginBottom: 24,
-  },
+    root: {
+        marginBottom: 24,
+    },
 }));
 
 export default function TodoItemForm() {
-  const classes = useInputStyles();
-  const { dispatch } = useTodoItems();
-  const { control, handleSubmit, reset, watch } = useForm();
+    const classes = useInputStyles();
+    const { dispatch } = useTodoItems();
+    const { control, handleSubmit, reset, watch } = useForm();
 
-  return (
-    <form
-      onSubmit={handleSubmit((formData) => {
-        dispatch({ type: "add", data: { todoItem: formData } });
-        reset({ title: "", details: "", tags: "" });
-      })}
-    >
-      <Controller
-        name="title"
-        control={control}
-        defaultValue=""
-        rules={{ required: true }}
-        render={({ field }) => (
-          <TextField
-            {...field}
-            label="TODO"
-            fullWidth={true}
-            className={classes.root}
-          />
-        )}
-      />
-      <Controller
-        name="details"
-        control={control}
-        defaultValue=""
-        render={({ field }) => (
-          <TextField
-            {...field}
-            label="Details"
-            fullWidth={true}
-            multiline={true}
-            className={classes.root}
-          />
-        )}
-      />
-      <Controller
-        name="tags"
-        control={control}
-        defaultValue=""
-        rules={{ required: false }}
-        render={({ field }) => (
-          <FormControl fullWidth={true}>
-            <InputLabel>Tag</InputLabel>
-            <Select {...field} label="Tags">
-              <MenuItem value={"work"}>work</MenuItem>
-              <MenuItem value={"personal"}>personal</MenuItem>
-              <MenuItem value={"else"}>else</MenuItem>
-              <MenuItem value={""}>no tag</MenuItem>
-            </Select>
-          </FormControl>
-        )}
-      />
-      <Button
-        variant="contained"
-        color="primary"
-        type="submit"
-        disabled={!watch("title")}
-      >
-        Add
-      </Button>
-    </form>
-  );
+    return (
+        <form
+            onSubmit={handleSubmit((formData) => {
+                dispatch({ type: 'add', data: { todoItem: formData } });
+                reset({ title: '', details: '', tags: '' });
+            })}
+        >
+            <Controller
+                name="title"
+                control={control}
+                defaultValue=""
+                rules={{ required: true }}
+                render={({ field }) => (
+                    <TextField
+                        {...field}
+                        label="TODO"
+                        fullWidth={true}
+                        className={classes.root}
+                    />
+                )}
+            />
+            <Controller
+                name="details"
+                control={control}
+                defaultValue=""
+                render={({ field }) => (
+                    <TextField
+                        {...field}
+                        label="Details"
+                        fullWidth={true}
+                        multiline={true}
+                        className={classes.root}
+                    />
+                )}
+            />
+            <Controller
+                name="tags"
+                control={control}
+                defaultValue=""
+                rules={{ required: false }}
+                render={({ field }) => (
+                    <FormControl fullWidth={true}>
+                        <InputLabel>Tag</InputLabel>
+                        <Select
+                        {...field}
+                        label="Tags"
+                        >
+                            <MenuItem value={'work'}>work</MenuItem>
+                            <MenuItem value={'personal'}>personal</MenuItem>
+                            <MenuItem value={'else'}>else</MenuItem>
+                            <MenuItem value={''}>no tag</MenuItem>
+                        </Select>
+                    </FormControl>
+                )}
+            />
+            <Button
+                variant="contained"
+                color="primary"
+                type="submit"
+                disabled={!watch('title')}
+            >
+                Add
+            </Button>
+        </form>
+    );
 }
--- src/TodoItems.tsx
+++ src/TodoItems.tsx
@@ -1,152 +1,161 @@
-import { useCallback, useState, useEffect } from "react";
-import Card from "@material-ui/core/Card";
-import CardHeader from "@material-ui/core/CardHeader";
-import CardContent from "@material-ui/core/CardContent";
-import Typography from "@material-ui/core/Typography";
-import IconButton from "@material-ui/core/IconButton";
-import Checkbox from "@material-ui/core/Checkbox";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import DeleteIcon from "@material-ui/icons/Delete";
-import Select from "@material-ui/core/Select";
-import MenuItem from "@material-ui/core/MenuItem";
-import FormControl from "@material-ui/core/FormControl";
-import InputLabel from "@material-ui/core/InputLabel";
-import { makeStyles } from "@material-ui/core/styles";
-import classnames from "classnames";
-import { motion } from "framer-motion";
-import { TodoItem, useTodoItems } from "./TodoItemsContext";
+import { useCallback, useState, useEffect } from 'react';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import DeleteIcon from '@material-ui/icons/Delete'; 
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import { makeStyles } from '@material-ui/core/styles';
+import classnames from 'classnames';
+import { motion } from 'framer-motion';
+import { TodoItem, useTodoItems } from './TodoItemsContext';
 
 const spring = {
-  type: "spring",
-  damping: 25,
-  stiffness: 120,
-  duration: 0.25,
+    type: 'spring',
+    damping: 25,
+    stiffness: 120,
+    duration: 0.25,
 };
 
 const useTodoItemListStyles = makeStyles({
-  root: {
-    listStyle: "none",
-    padding: 0,
-  },
+    root: {
+        listStyle: 'none',
+        padding: 0,
+    },
 });
 
 export const TodoItemsList = function () {
-  const { todoItems } = useTodoItems();
-  const classes = useTodoItemListStyles();
 
-  const [SortedItems, SetSortedItems] = useState<Array<any> | any>([]);
-  const [tag, setTag] = useState<string>("");
+    const { todoItems } = useTodoItems();
+    const classes = useTodoItemListStyles();
 
-  const sortedItems = [...todoItems].slice().sort((a, b) => {
-    if (a.done && !b.done) {
-      return 1;
+
+    const [SortedItems, SetSortedItems] = useState<Array<any> | any>([])
+    const [tag, setTag] = useState<string>('')
+
+    const sortedItems = [...todoItems].slice().sort((a, b) => {
+        if (a.done && !b.done) {
+            return 1;
+        }
+
+        if (!a.done && b.done) {
+            return -1;
+        }
+
+        return 0;
+    });
+
+    function filterArr (todoItems: any, tag: string) {
+        if (tag === '') return todoItems
+        return (
+            todoItems.filter(
+                (a: any) => a.tags === tag
+            )) as any;
     }
 
-    if (!a.done && b.done) {
-      return -1;
-    }
+    const handleChange = (event: any) => {
+        setTag(event.target.value as string);
+    };
 
-    return 0;
-  });
+    useEffect(() => {
+        SetSortedItems(filterArr([...sortedItems], tag))
+    }, [todoItems, tag])
 
-  function filterArr(todoItems: any, tag: string) {
-    if (tag === "") return todoItems;
-    return todoItems.filter((a: any) => a.tags === tag) as any;
-  }
 
-  const handleChange = (event: any) => {
-    setTag(event.target.value as string);
-  };
-
-  useEffect(() => {
-    SetSortedItems(filterArr([...sortedItems], tag));
-  }, [todoItems, tag]);
-
-  return (
-    <div>
-      <FormControl fullWidth={true}>
-        <InputLabel>Filter tag</InputLabel>
-        <Select onChange={handleChange} disableUnderline>
-          <MenuItem value={"work"}>work</MenuItem>
-          <MenuItem value={"personal"}>personal</MenuItem>
-          <MenuItem value={"else"}>else</MenuItem>
-          <MenuItem value={""}>no filter</MenuItem>
-        </Select>
-      </FormControl>
-      <ul className={classes.root}>
-        {SortedItems.map((item: any) => (
-          <motion.li key={item.id} transition={spring} layout={true}>
-            <TodoItemCard item={item} />
-          </motion.li>
-        ))}
-      </ul>
-    </div>
-  );
+    return (
+        <div>
+            <FormControl fullWidth={true}>
+                        <InputLabel>Filter tag</InputLabel>
+                        <Select 
+                        onChange={handleChange}
+                        disableUnderline
+                        >
+                            <MenuItem value={'work'}>work</MenuItem>
+                            <MenuItem value={'personal'}>personal</MenuItem>
+                            <MenuItem value={'else'}>else</MenuItem>
+                            <MenuItem value={''}>no filter</MenuItem>
+                        </Select>
+                    </FormControl>
+            <ul className={classes.root}>
+                {SortedItems.map((item:any) => (
+                    <motion.li key={item.id} transition={spring} layout={true}>
+                        <TodoItemCard item={item} />
+                    </motion.li>
+                ))}
+            </ul>
+        </div>
+    );
 };
 
 const useTodoItemCardStyles = makeStyles({
-  root: {
-    marginTop: 24,
-    marginBottom: 24,
-  },
-  doneRoot: {
-    textDecoration: "line-through",
-    color: "#888888",
-  },
+    root: {
+        marginTop: 24,
+        marginBottom: 24,
+    },
+    doneRoot: {
+        textDecoration: 'line-through',
+        color: '#888888',
+    },
 });
 
 export const TodoItemCard = function ({ item }: { item: TodoItem }) {
-  const classes = useTodoItemCardStyles();
-  const { dispatch } = useTodoItems();
+    const classes = useTodoItemCardStyles();
+    const { dispatch } = useTodoItems();
 
-  const handleDelete = useCallback(
-    () => dispatch({ type: "delete", data: { id: item.id } }),
-    [item.id, dispatch]
-  );
+    const handleDelete = useCallback(
+        () => dispatch({ type: 'delete', data: { id: item.id } }),
+        [item.id, dispatch],
+    );
 
-  const handleToggleDone = useCallback(
-    () =>
-      dispatch({
-        type: "toggleDone",
-        data: { id: item.id },
-      }),
-    [item.id, dispatch]
-  );
+    const handleToggleDone = useCallback(
+        () =>
+            dispatch({
+                type: 'toggleDone',
+                data: { id: item.id },
+            }),
+        [item.id, dispatch],
+    );
 
-  return (
-    <Card
-      className={classnames(classes.root, {
-        [classes.doneRoot]: item.done,
-      })}
-    >
-      <CardHeader
-        action={
-          <IconButton aria-label="delete" onClick={handleDelete}>
-            <DeleteIcon />
-          </IconButton>
-        }
-        title={
-          <FormControlLabel
-            control={
-              <Checkbox
-                checked={item.done}
-                onChange={handleToggleDone}
-                name={`checked-${item.id}`}
-                color="primary"
-              />
-            }
-            label={item.title}
-          />
-        }
-      />
-      {item.details ? (
-        <CardContent>
-          <Typography variant="body2" component="p">
-            {item.details}
-          </Typography>
-        </CardContent>
-      ) : null}
-      {item.tags}
-    </Card>
-  );
+    return (
+        <Card
+            className={classnames(classes.root, {
+                [classes.doneRoot]: item.done,
+            })}
+        >
+            <CardHeader
+                action={
+                    <IconButton aria-label="delete" onClick={handleDelete}>
+                        <DeleteIcon />
+                    </IconButton>
+                }
+                title={
+                    <FormControlLabel
+                        control={
+                            <Checkbox
+                                checked={item.done}
+                                onChange={handleToggleDone}
+                                name={`checked-${item.id}`}
+                                color="primary"
+                            />
+                        }
+                        label={item.title}
+                    />
+                }
+            />
+            {item.details ? (
+                <CardContent>
+                    <Typography variant="body2" component="p">
+                        {item.details}
+                    </Typography>
+                </CardContent>
+            ) : null}
+            {item.tags}
+        </Card>
+    );
 };
